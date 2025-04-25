cbe69d9ff63ed0cdf81b389de10e49f79b7f2592
Bidhu1024 <bidhu1024av@gmail.com>
2025-04-25 22:42:02 +0530

    344

--- TwoPointers/ReverseString.js
+++ TwoPointers/ReverseString.js
@@ -11,3 +11,17 @@ return finalArr
 };
 
 console.log(reverseString(["H","a","n","n","a","h"]))
+ // two pointer approach
+var reverseString = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        // Swap characters
+        [s[left], s[right]] = [s[right], s[left]];
+
+        // Move the pointers
+        left++;
+        right--;
+    }
+};
