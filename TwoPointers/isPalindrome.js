3dedbe6800e9f436e55dd7512313a81cde567fef
Bidhu1024 <bidhu1024av@gmail.com>
2025-04-26 22:56:03 +0530

    125

---
+++ TwoPointers/isPalindrome.js
@@ -0,0 +1,16 @@
+var isPalindrome = function (s) {
+  let newStr = s.toLowerCase();
+
+  newStr =  newStr.replace(/[^a-z0-9]/g, "");
+
+  let left = 0;
+  let right = newStr.length - 1;
+while(left <= right){
+    if (newStr[left] !== newStr[right]) return false;
+    left++;
+    right--;
+}
+return true
+};
+
+console.log(isPalindrome("race a car"));
