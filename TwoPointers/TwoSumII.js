02ae16c4f26b35dced952d839e9a6f876511050d
Bidhu1024 <bidhu1024av@gmail.com>
2025-04-30 09:40:42 +0530

    167

---
+++ TwoPointers/TwoSumII.js
@@ -0,0 +1,33 @@
+
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    //solved in brute-force since no with tc - o(n^2) and sc - 0(1)
+
+
+//   for(let i = 0;i<numbers.length;i++){
+//     for(let j = i+1;j<numbers.length;j++){
+//         if(numbers[i]+numbers[j] === target){
+//             return [i+1,j+1]
+//         }
+//     }
+//   }  
+
+
+let left = 0;
+let right = nums.length - 1;
+
+while(left < right){
+    if(nums[left] + nums[right] === target){
+        return [left+1,right+1] // since it is 1 index based 
+    }else if(nums[left] + nums[right] < target){
+        left++;
+    }else {
+        right--
+    }
+}
+};
