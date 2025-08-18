4725cf33ef0270a159aa491fcd9bfbb6befece12
Bidhu1024 <bidhu1024av@gmail.com>
2025-08-18 09:14:01 +0530

    3sum doubt

---
+++ TwoPointers/3Sum.js
@@ -0,0 +1,22 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number[][]}
+//  */
+// var threeSum = function(nums) {
+//     if(nums.length <=2) return []
+//     const arr = []
+// for(let i = 0 ;i<nums.length;i++ ){
+//     for(let j = i+1;i<nums.length;j++){
+//         for(let k = i+2;i<nums.length;k++){
+//             if(nums[i]+nums[j]+nums[k] === 0){
+//                 arr.push([nums[i],nums[j],nums[k]])
+               
+//             }
+//         }
+//     }
+//     return arr
+// }
+
+  
+// };
+// console.log(threeSum([-1,0,1,2,-1,-4])) blunders😖😖😖😖
